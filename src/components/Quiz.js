d495ee07c8c8044da768860207d45bcb3e0b6dfc
OrynaBolychova <orynabolychova@gmail.com>
2024-10-04 23:54:20 +0300

    State management

---
+++ src/components/Quiz.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import Question from "./Question";
+import { QuizContext } from "./contexts/quiz";
+
+const Quiz = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+  console.log("quizState", quizState);
+
+  return (
+    <div className="quiz">
+      <div>
+        <div className="score">Question 1/8</div>
+        <Question />
+        <div
+          className="next-button"
+          onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+        >
+          Next Question
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
